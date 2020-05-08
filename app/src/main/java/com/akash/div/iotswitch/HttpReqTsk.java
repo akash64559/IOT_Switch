bd57c0c71b78d4ec8e7afdc07864687261c6e455
akash64559 <akash64559gupta@gmail.com>
2020-05-08 21:34:50 +0530

    push test

--- app/src/main/java/com/akash/div/iotswitch/HttpReqTsk.java
+++ app/src/main/java/com/akash/div/iotswitch/HttpReqTsk.java
@@ -1,18 +1,17 @@
 package com.akash.div.iotswitch;
 
 import android.os.AsyncTask;
-import android.util.Log;
+
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+
 
 class HTTPReqTask extends AsyncTask<Void, Void, Void> {
     @Override
---
+++ app/src/main/java/com/akash/div/iotswitch/MyWebSockets.java
@@ -0,0 +1,5 @@
+package com.akash.div.iotswitch;
+
+public class MyWebSockets {
+
+}
