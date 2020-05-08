34f891e5fcd96398b0b707ef4e36e3cc005051a1
akash64559 <akash64559gupta@gmail.com>
2020-05-08 23:08:11 +0530

    socket for local connection completed for now

---
+++ app/src/main/java/com/akash/div/iotswitch/MySockets.java
@@ -0,0 +1,60 @@
+package com.akash.div.iotswitch;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MySockets{
+    String ip;
+    int port;
+    Socket s;
+    PrintWriter out;
+    BufferedReader in;
+    public MySockets(String ip,int port){
+        this.ip=ip;
+        this.port=port;
+    }
+    public void connect(){
+        try {
+            s = new Socket(ip,port);
+            out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(s.getOutputStream())),
+                    true);
+            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String sendReceive(String msg) {
+        String read=null;
+        if(out!=null&&in!=null){
+            out.println(msg);
+            try {
+                read= in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return read;
+    }
+    public void close(){
+        if(s!=null){
+            try {
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public boolean isConnected(){
+        return s.isConnected();
+    }
+}
--- app/src/main/java/com/akash/div/iotswitch/MyWebSockets.java
+++
@@ -1,5 +0,0 @@
-package com.akash.div.iotswitch;
-
-public class MyWebSockets {
-
-}
